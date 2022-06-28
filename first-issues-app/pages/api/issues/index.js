a05d9f458aa8159d197374180bbc9278423dfdb1
Hopelezz <72772558+Hopelezz@users.noreply.github.com>
2022-06-28 12:50:07 -0500

    create new app shows first issues on github

---
+++ first-issues-app/pages/api/issues/index.js
@@ -0,0 +1,20 @@
+// api/issues
+
+import { Octokit } from "octokit";
+
+export default async function handle(req, res) {
+    const octokit = new Octokit();
+    const q = "is:open is:issue label:good-first-issue"
+
+    const response = await octokit.request("GET /search/issues", { q });
+    const results = response.data.items.map((items) => ({
+        name: items.title,
+        author: items.user.login,
+        labels: items.labels.map((label) =>label.name),
+        url: items.html_url
+    }));
+
+    const random = Math.floor(Math.random() * results.length + 1);
+
+    res.status(200).json(results[random]);
+}
