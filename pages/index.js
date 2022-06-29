5958ecc3986f4925f48f7429b4c5779cd10d9708
Hopelezz <72772558+Hopelezz@users.noreply.github.com>
2022-06-29 17:43:21 -0500

    issue #4. Corrected the CSS render of the BG

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ export default function Home({ data }) {
       <Head>
         <title>Daily Issues</title>
         <meta name="description" content="GitHub: Good-First-Issue" />
+        <meta name="viewport" content="width=device-width, user-scalable=no"></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
         <div className={styles.containerglass}>
